da1ae262eb7a632f713c8b84fa3fd730b019c2c6
bsj <920757938@qq.com>
2022-11-10 09:10:40 +0800

    fileChannelTest

---
+++ src/main/java/org/example/nio/FileChannelTest.java
@@ -0,0 +1,9 @@
+package org.example.nio;
+
+import java.nio.channels.FileChannel;
+
+public class FileChannelTest {
+    public static void main(String[] args) {
+        FileChannel inChannel = null;
+    }
+}
